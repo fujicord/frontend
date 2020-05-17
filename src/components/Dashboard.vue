b1ac04bccb73a9c6f7cf1136264678d9ed000c53
lepeli <yvan@lepeli.fr>
2020-05-17 03:07:08 +0200

    changes

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="dashboard">
+      <NotLoggedIn></NotLoggedIn>
+  </div>
+</template>
+
+<script>
+import NotLoggedIn from '@/components/NotLoggedIn.vue'
+export default {
+    data(){
+        return {
+            logged: false
+        }
+    },
+    components: {NotLoggedIn}
+}
+</script>
+
+<style>
+/* fOND: 2C2F33 ; Fond pour les champs: #23272A */
+body{
+    background: #2C2F33;
+}
+</style>
---
+++ src/components/NotLoggedIn.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="login">
+        <div class="container">
+            <div class="row">
+                <div class="col-md"></div>
+                <div class="col-md-10 col-sm text-center">
+                    <div class="login_required">You need to log in with Discord to manage the bot</div>
+                    <b-btn @click="login()" class="login">Login with discord</b-btn>
+                </div>
+                <div class="col-md"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        login(){
+            this.$store.commit('login')
+        }
+    }
+}
+</script>
+
+<style>
+.login_required{
+    color: white;
+}
+#login{
+    padding-top: 70px;
+}
+.login{
+    margin-top: 20px;
+}
+</style>
